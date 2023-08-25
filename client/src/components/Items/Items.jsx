6304110412bc77af12c67f6ce0a00d457764735f
Def <deferenseo@gmail.com>
2023-08-25 20:33:14 +0300

    created thunk(getItems)/hooked up axios(created request)/added extraReducers(cases)

---
+++ client/src/components/Items/Items.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItems } from '../../store/items/itemsSlice';
+import Spinner from '../Spinner/Spinner';
+import Item from '../Item/Item';
+import ContentWrapper from '../ContentWrapper/ContentWrapper';
+
+const Items = (props) => {
+    const dispatch = useDispatch()
+    const { items, isLoading } = useSelector(state => state.items)
+
+    useEffect(() => {
+        dispatch(getItems())
+    }, [dispatch])
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    return (
+        <div>
+            <ContentWrapper>
+                {items && items.map(item => <Item item={item} />)}
+            </ContentWrapper>
+
+        </div>
+    );
+}
+
+export default Items;
---
+++ client/src/store/sevises/itemsService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const getItems = async () => {
+    const items = await axios.get("/api/items")
+    return items.data
+}
+
+const itemsService = {
+    getItems
+}
+
+export default itemsService;
